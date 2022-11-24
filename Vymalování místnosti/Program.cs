647429542d898b0e1c862c668acec9a242f711a5
drozboril <rozborildavid77@gmail.com>
2022-11-24 18:11:38 +0100

    Uživatel zadá rozměry stěn v místnosti, program vypočítá množství potřebné barvy v ml a kg

---
+++ Vymalování místnosti/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vymalování_místnosti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte délku, šířku a výšku stěn místnosti v m^2");
+            int d = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            int v = int.Parse(Console.ReadLine());
+            double MalovanaPlocha = 2 * v * (s + d) + (s * d);
+            double spotrebaml = MalovanaPlocha * 160 / 1000;
+            double spotrebakg = MalovanaPlocha * 220 / 1000;
+            Console.WriteLine("Na vymalování místnosti je potřeba {0} ml barvy, neboli {1} kg", spotrebaml, spotrebakg);
+            Console.ReadLine();
+        }
+    }
+}
